a91435192fa38115b0a7e3b0a36179107a2452d6
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-06-15 20:38:21 -0400

    resolving errors in vercel logs to host front end

--- components/City.js
+++ components/City.js
@@ -8,7 +8,7 @@ function City({ categories, setCategories, city, cityKeys }) {
     if (Object.keys(cityKeys).includes("on_water")) {
         city.on_water=city.on_water.toString()
     }
-    const cityData = cityKeys.map((k)=><td className="p-4 px-6 text-xs align-middle border-t-0 border-l-0 border-r-0 whitespace-nowrap">{city[k].toString()}</td>)
+    const cityData = cityKeys.map((k)=><td className="p-4 px-6 text-xs align-middle border-t-0 border-l-0 border-r-0 whitespace-nowrap" key={city[k].toString()}>{city[k].toString()}</td>)
     return (
         <tr>
             <th className="flex items-center p-4 px-6 text-xs text-left align-middle border-t-0 border-l-0 border-r-0 whitespace-nowrap">
